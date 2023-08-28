9244b50ef5662a2308c3dc1fa2dfbca8ab2d90fa
Dilan Marcano <d.marcano@DAKA.LOCAL>
2023-08-28 19:50:24 -0400

    28-08-2023 19:15 definir estructura del proyecto

--- src/routes/main.route.js
+++ src/routes/main.route.js
@@ -2,13 +2,13 @@ const router = require("express").Router();
 const { dataMySql } = require("../controllers/main.controller");
 
 router.get("/", async (req, res) => {
+  res.render("index");
+});
+
+router.get("/mysql", async (req, res) => {
     const rta = await  dataMySql();
     res.json(rta);
     return rta;
-});
-
-router.get("/index", async (req, res) => {
-  res.render("index");
 });
 
 module.exports = router;
